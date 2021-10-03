a503ede2cb980ea4f0a5c405ccd4ae1d8f568fa1
pat-ern <pat.villarroel@duocuc.cl>
2021-10-03 00:58:53 -0300

    prueba

--- desarrollo actividad 2_1_5.sql
+++ desarrollo actividad 2_1_5.sql
@@ -1,18 +1,18 @@
 --caso 1
 
-SELECT 
+SELECT
 carreraid as "IDENTIFICACION DE LA CARRERA",
 COUNT(*) as "TOTAL ALUMNOS MATRICULADOS",
 'Le corresponden '||TO_CHAR(COUNT(*)*&ASIGNACION, 'FML999G999')||' del presupuesto total asignado para publicidad' as "MONTO POR PUBLICIDAD"
 FROM alumno
 GROUP BY carreraid
-ORDER BY 
-"TOTAL ALUMNOS MATRICULADOS" DESC, 
+ORDER BY
+"TOTAL ALUMNOS MATRICULADOS" DESC,
 "IDENTIFICACION DE LA CARRERA" DESC;
 
--- caso 2
+-- caso 2er
 
-SELECT 
+SELECT
 carreraid AS CARRERA,
 COUNT(*) AS "TOTAL DE ALUMNOS"
 FROM alumno
@@ -22,7 +22,7 @@ ORDER BY CARRERA;
 
 -- caso 3
 
-SELECT 
+SELECT
 TO_CHAR(run_jefe, '09G999G999') AS "RUN JEFE SIN DV",
 COUNT(*) AS "TOTAL DE EMPLEADOS A SU CARGO",
 TO_CHAR(MAX(salario), '9G999G999') AS "SALARIO MAXIMO",
@@ -36,7 +36,7 @@ ORDER BY COUNT(*);
 
 -- caso 4
 
-SELECT 
+SELECT
 id_escolaridad AS ESCOLARIDAD,
 CASE id_escolaridad
     WHEN 10 THEN 'BÁSICA'
@@ -65,7 +65,7 @@ CASE
     WHEN COUNT(*) BETWEEN 2 AND 3 THEN 'Se requiere comprar 1 nuevo ejemplar'
     WHEN COUNT(*) BETWEEN 4 AND 5 THEN 'Se requieren comprar 2 nuevos ejemplares'
     ELSE 'Se requieren comprar 4 nuevos ejemplares'
-END AS Sugerencia    
+END AS Sugerencia
 FROM prestamo
 WHERE EXTRACT(YEAR FROM fecha_ini_prestamo) = EXTRACT(YEAR FROM SYSDATE)-1
 GROUP BY tituloid
@@ -80,14 +80,14 @@ SELECT
     TO_CHAR(COUNT(*)*10000, 'FML999G999') AS BONO
 FROM prestamo
 
-WHERE 
+WHERE
     EXTRACT(YEAR FROM fecha_ini_prestamo) = EXTRACT(YEAR FROM SYSDATE)-1
 
-GROUP BY 
-    run_emp, 
+GROUP BY
+    run_emp,
     TO_CHAR(fecha_ini_prestamo, 'mm/yyyy')
 
-ORDER BY 
-    fecha, 
+ORDER BY
+    fecha,
     COUNT(*)*10000 DESC,
     run_emp DESC;
